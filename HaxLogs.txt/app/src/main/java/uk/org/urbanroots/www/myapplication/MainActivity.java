2255a29de932006e1e486a33a01191c9a7e9d986
Lampros Valais <lvalais@gmail.com>
2015-11-07 14:46:55 +0000

    Button test

--- HaxLogs.txt/app/src/main/java/uk/org/urbanroots/www/myapplication/MainActivity.java
+++ HaxLogs.txt/app/src/main/java/uk/org/urbanroots/www/myapplication/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +26,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        handleOpenMenu();
+    }
+
+    private void handleOpenMenu(){
+        Button buttonOpenMenu = (Button)findViewById(R.id.button);
+        buttonOpenMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button!", Toast.LENGTH_LONG).show();
             }
         });
     }
